5b85107b9f4db37d67e0adb23066eccc14512f64
Anurag <anuragkashyap275@gmail.com>
2017-07-08 13:27:53 +0530

    Initial Commit

---
+++ app/src/main/java/anurag/ui/MainActivity.java
@@ -0,0 +1,29 @@
+package anurag.ui;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import static anurag.ui.R.layout.activity_main;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,"yoo",Snackbar.LENGTH_LONG).setAction("redo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                    }
+                }).show();
+            }
+        });
+    }
+}
